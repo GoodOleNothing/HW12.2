f19e2aa46e249181014b3c68c9977a3a05e62485
GoodOleNothing <alexgog77777@gmail.com>
2023-02-13 09:34:41 +0500

    Дописаны тесты для 100% покрытия. Фикстура работать отказалась

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,3 +1,5 @@
+import pytest
+
 from utils import arrs
 
 
@@ -6,6 +8,18 @@ def test_get():
     assert arrs.get([], -1, "test") == "test"
 
 
+def coll():
+    return [[1, 2, 3, 4], [1, 2, 3], []]
+def start():
+    return (0, 1, -1, -4, None)
+def end():
+    return (3, None)
+
+
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([]) == []
+    assert arrs.my_slice([1, 2, 3, 4], -1) == [4]
+    assert arrs.my_slice([1, 2, 3], -4) == [1,2,3]
+
